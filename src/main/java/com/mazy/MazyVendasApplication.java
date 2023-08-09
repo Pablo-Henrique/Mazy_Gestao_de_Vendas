8ddd394c22683c448b50d2db36a9b465204cebe7
Pablo <pablo@ti.superpinheirao.interno>
2023-08-09 09:28:53 -0300

    initial commit

---
+++ src/main/java/com/mazy/MazyVendasApplication.java
@@ -0,0 +1,13 @@
+package com.mazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazyVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazyVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mazy/MazyVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazyVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
